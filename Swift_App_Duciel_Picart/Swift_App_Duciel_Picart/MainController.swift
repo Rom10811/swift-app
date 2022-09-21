b8d3907ee3bc7685f282f990615fa627d343df35
etudiant <etudiant@MacBook-Air-de-Administrateur.local>
2022-09-21 17:10:00 +0200

    authentification avec firebase

--- Swift_App_Duciel_Picart/Swift_App_Duciel_Picart/MainController.swift
+++ Swift_App_Duciel_Picart/Swift_App_Duciel_Picart/MainController.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainController: UIViewController {
 
+    @IBOutlet weak var Mail: UITextField!
+    @IBOutlet weak var Password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func onClick(_ sender: Any) {
-        print("click")
+    @IBAction func onClickSignInButton(_ sender: Any) {
+        let email: String = Mail.text ?? ""
+        let password: String = Password.text ?? ""
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+          print(strongSelf)
+        }
     }
 }
 
---
+++ Swift_App_Duciel_Picart/Swift_App_Duciel_Picart/SignUpController.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpController.swift
+//  Swift_App_Duciel_Picart
+//
+//  Created by etudiant on 21/09/2022.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpController: UIViewController {
+
+    @IBOutlet weak var Mail: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onClickSignUpButton(_ sender: Any) {
+        let email: String = Mail.text ?? ""
+        let password: String = Password.text ?? ""
+        Auth.auth().createUser(withEmail: email, password: password)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
